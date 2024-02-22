3b35accf02ec6edc10ccf3c66fc0512c2aa1ba9d
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-02-22 19:36:29 +0100

    Splash.kt modified

--- app/src/main/java/com/example/m8_projecte_f1_2/Splash.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Splash.kt
@@ -1,11 +1,22 @@
 package com.example.m8_projecte_f1_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Splash : AppCompatActivity() {
+    private val duracio: Long=3000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        //amaguem la barra, pantalla a full
+        supportActionBar?.hide()
+        canviarActivity();
+    }
+    private fun canviarActivity(){
+        Handler().postDelayed({
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
