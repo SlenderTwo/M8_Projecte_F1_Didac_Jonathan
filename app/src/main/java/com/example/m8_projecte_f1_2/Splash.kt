ff01c80318cc3fe9972990853c50ce0f1f69fae6
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-02-23 19:18:27 +0100

    Alonso y su sonidito añadido

--- app/src/main/java/com/example/m8_projecte_f1_2/Splash.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Splash.kt
@@ -3,22 +3,50 @@ package com.example.m8_projecte_f1_2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.media.MediaPlayer
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class Splash : AppCompatActivity() {
-    private val duracio: Long=3000;
+    private val duracio: Long=10000
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         //amaguem la barra, pantalla a full
         supportActionBar?.hide()
-        canviarActivity();
-    }
-    private fun canviarActivity(){
-        Handler().postDelayed({
-            val intent= Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        } , duracio)
 
-}
+        // Inicializar el MediaPlayer con el archivo de música
+        mediaPlayer = MediaPlayer.create(this, R.raw.f1)
+
+        // Reproducir la música
+        mediaPlayer.start()
+
+        //cridem a la funció de canviar activitat
+        canviarActivity()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Liberar recursos del MediaPlayer
+        mediaPlayer.release()
+    }
+
+    private fun canviarActivity(){
+        Timer().schedule(duracio){
+            saltainici()
+        }
+    }
+
+    private fun saltainici()
+    {
+        val intent=Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        // Detener la música cuando iniciamos la próxima actividad
+        mediaPlayer.stop()
+        // Liberar recursos del MediaPlayer
+        mediaPlayer.release()
+        finish()
+    }
 }
