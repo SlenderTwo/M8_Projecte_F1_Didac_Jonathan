215a68ebca79ab27cf9439b69d1c2ecf77e2bd63
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-02-28 18:24:03 +0100

    Creacion de usuarios en el Firebase

--- app/src/main/java/com/example/m8_projecte_f1_2/Registre.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Registre.kt
@@ -3,10 +3,13 @@ package com.example.m8_projecte_f1_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import java.text.DateFormat.getDateInstance
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -20,35 +23,81 @@ class Registre : AppCompatActivity() {
     lateinit var nombreEt :EditText
     lateinit var fechaTxt :TextView
     lateinit var Registrar :Button
+    lateinit var auth: FirebaseAuth //FIREBASE AUTENTIFICACIO
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registre)
 
         // Busquem a R els elements als que apunten les variables
-        correoEt =findViewById<EditText>(R.id.correoEt)
-        passEt =findViewById<EditText>(R.id.passEt)
-        nombreEt =findViewById<EditText>(R.id.nombreEt)
-        fechaTxt =findViewById<TextView>(R.id.fechaEt)
-        Registrar =findViewById<Button>(R.id.Registrar)
+        correoEt = findViewById<EditText>(R.id.correoEt)
+        passEt = findViewById<EditText>(R.id.passEt)
+        nombreEt = findViewById<EditText>(R.id.nombreEt)
+        fechaTxt = findViewById<TextView>(R.id.fechaEt)
+        Registrar = findViewById<Button>(R.id.Registrar)
 
-        Log.d("Jonathan","Inici")
 
         //carreguem la data al TextView
         //Utilitzem calendar (hi ha moltes altres opcions)
         val date = Calendar.getInstance().time
-        Log.d("Jonathan",date.toString())
-        Toast.makeText(this, date.toString(),Toast.LENGTH_LONG).show();
-
         val formatter = SimpleDateFormat.getDateInstance() //or use getDateInstance()
         val formatedDate = formatter.format(date)
         //ara la mostrem al TextView
         fechaTxt.setText(formatedDate)
 
+        //Instanciem el firebaseAuth
+        auth = FirebaseAuth.getInstance()
 
-
-
+        Registrar.setOnClickListener() {
+            //Abans de fer el registre validem les dades
+            33
+            var email: String = correoEt.getText().toString()
+            var pass: String = passEt.getText().toString()
+            // validació del correu
+            // si no es de tipus correu
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                correoEt.setError("Invalid Mail")
+            } else if (pass.length <6) {
+                passEt.setError("Password less than 6 chars")
+            } else {
+                RegistrarJugador(email, pass)
+            }
+        }
     }
-
+    fun RegistrarJugador(email:String, passw:String){
+        auth.createUserWithEmailAndPassword(email, passw)
+            .addOnCompleteListener(this) { task ->
+                34
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        this,"createUserWithEmail:success",Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+    fun updateUI(user: FirebaseUser?){
+        //hi ha un interrogant perquè podria ser null
+        if (user!=null)
+        {
+            var puntuacio: Int = 0
+            var uidString: String = user.uid
+            var correoString: String = correoEt.getText().toString()
+            var passString: String = passEt.getText().toString()
+            var nombreString: String = nombreEt.getText().toString()
+            var fechaString: String= fechaTxt.getText().toString()
+            //AQUI GUARDA EL CONTINGUT A LA BASE DE DADES
+// FALTA FER
+        }
+        else
+        {
+            Toast.makeText( this,"ERROR CREATE USER",Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
