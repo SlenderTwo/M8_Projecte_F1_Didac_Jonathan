3313e0cc2f356a77019bc3c4f61857d4853ba8e4
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-03-05 19:48:16 +0100

    Hasta la pagina 53

--- app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
@@ -1,21 +1,61 @@
 package com.example.m8_projecte_f1_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class Menu : AppCompatActivity() {
     //creem unes variables per comprovar ususari i authentificació
     lateinit var auth: FirebaseAuth
+    lateinit var tancarSessio: Button
+
     var user: FirebaseUser? = null;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
         auth= FirebaseAuth.getInstance()
         user =auth.currentUser
+        tancarSessio =findViewById<Button>(R.id.tancarSessio)
+        tancarSessio.setOnClickListener(){
+            tancalaSessio()
+        }
+
 
     }
+    private fun tancalaSessio() {
+        auth.signOut() //tanca la sessió
+        //va a la pantalla inicial
+        val intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    // Aquest mètode s'executarà quan s'obri el minijoc
+
+    override fun onStart() {
+        usuariLogejat()
+        super.onStart()
+    }
+    private fun  usuariLogejat()
+    {
+        if (user !=null)
+        {
+            Toast.makeText(this,"Jugador logejat",
+                Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
 
 }
