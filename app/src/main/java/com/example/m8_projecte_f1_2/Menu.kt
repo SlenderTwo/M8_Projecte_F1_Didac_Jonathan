d7001c7ec4041645bfdd26b73cd67f99e7eb47ee
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-03-05 19:25:56 +0100

    Fase 4 empezada

--- app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
@@ -2,10 +2,20 @@ package com.example.m8_projecte_f1_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Menu : AppCompatActivity() {
+    //creem unes variables per comprovar ususari i authentificació
+    lateinit var auth: FirebaseAuth
+    var user: FirebaseUser? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        auth= FirebaseAuth.getInstance()
+        user =auth.currentUser
+
     }
+
 }
