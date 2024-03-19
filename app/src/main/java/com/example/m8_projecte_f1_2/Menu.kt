4ca1439c187b8c0e3140735ba90d7243e64138dd
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-03-19 19:07:25 +0100

    Hasta la pagina 69

--- app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/Menu.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Menu : AppCompatActivity() {
     //creem unes variables per comprovar ususari i authentificació
@@ -23,6 +29,10 @@ class Menu : AppCompatActivity() {
     lateinit var uid: TextView
     lateinit var correo: TextView
     lateinit var nom: TextView
+
+    //reference serà el punter que ens envia a la base de dades de jugadors
+    lateinit var reference: DatabaseReference
+
 
     var user: FirebaseUser? = null;
 
@@ -104,6 +114,65 @@ class Menu : AppCompatActivity() {
         }
     }
 
+    private fun consulta(){
+        //Amb Firebase no fem consultes realment, el que fem en connectar-nos a una referencia
+                // i aquesta anirà canviant automàticament quan canvii la base de dades
+        // reference apunta a "DATA BASE JUGADORS"
+        // sempre es crea un referencia a un punt del arbre de la base de dades
+        // Per exemple si tenim la base de dades
+        // arrel
+        // - nivell dos
+        // - nivell dos.1
+// - nom: "pepe"
+        // - dni: "1231212121"
+        // - nivell dos.2: "34"
+        // - nivell dos.3: "36"
+        //var bdasereference:DatabaseReference = FirebaseDatabase.getInstance().getReference()
+        // .child("nivell dos")
+        // .child("nivell dos.1")
+        // Ara estariem al novell dos.1 del arbre
+        // Ens podem subscriure amb un listener que té dos métodes
+        // onDataChange (es crida si s'actualitza les dades o és la primera vegada que ens suscribim)
+        // onCancelled Es crida si hi ha un error o es cancel·la la lectura
+        // al primer métode rebrem un objecte json que és la branca sobre la que demanem actualització
+                // getkey retorna la clave del objecte
+                // getValue retorna el valor
+                // els subnodes (fills) es recuperen amb getChildren
+                // es poden llegir com un llistat d'objectes Datasnapshots
+        // o navegar a subnodes concrets amb child("nomdelsubnode")
+        var database: FirebaseDatabase = FirebaseDatabase.getInstance("https://montserratak-76f14-defaultrtdb.europe-west1.firebasedatabase.app/")
+        var bdreference:DatabaseReference = database.getReference("DATA BASE JUGADORS")
+        bdreference.addValueEventListener (object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                Log.i ("DEBUG","arrel value"+
+                        snapshot.getValue().toString())
+                Log.i ("DEBUG","arrel key"+ snapshot.key.toString())
+                // ara capturem tots els fills
+                var trobat:Boolean =false
+                for (ds in snapshot.getChildren()) {
+                    Log.i ("DEBUG","DS key:"+ds.child("Uid").key.toString())
+                    Log.i ("DEBUG","DS value:"+ds.child("Uid").getValue().toString())
+                    Log.i ("DEBUG","DS data:"+ds.child("Data").getValue().toString())
+                    Log.i ("DEBUG","DS mail:"+ds.child("Email").getValue().toString())
+                    //mirem si el mail és el mateix que el del jugador
+                    //si és així, mostrem les dades als textview corresponents
+                    if (ds.child("Email").getValue().toString().equals(user?.email)){
+                        trobat=true
+                    } //carrega els textview
+                        puntuacio.setText(ds.child("Puntuacio").getValue().toString())
+                        uid.setText(ds.child("Uid").getValue().toString())
+                        correo.setText(ds.child("Email").getValue().toString())
+                        nom.setText(ds.child("Nom").getValue().toString())
+                }
+                if (!trobat)
+                {
+                    Log.e ("ERROR","ERROR NO TROBAT MAIL")
+                }
+            }
+        }
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e ("ERROR","ERROR DATABASE CANCEL")
+        })
+        }
 
-
-}
+    }
