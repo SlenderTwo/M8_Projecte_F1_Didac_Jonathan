1971699a6d7b20349c6dca3d0b191ae7099914bf
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-04-01 20:25:31 +0200

    HAsta la pagina 113 pero sin el juego añadido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,7 @@ dependencies {
     // For example, add the dependencies for Firebase Authentication and Cloud Firestore
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
+
+    //picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
 }
