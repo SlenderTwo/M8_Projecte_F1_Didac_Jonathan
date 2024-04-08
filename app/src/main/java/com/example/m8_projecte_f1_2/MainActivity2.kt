e74291e3db80c684bff453061f3ed6f697087b1b
SlenderTwo <jonathan.tabres2222@gmail.com>
2024-04-08 14:39:47 +0200

    SoundPool

--- app/src/main/java/com/example/m8_projecte_f1_2/MainActivity2.kt
+++ app/src/main/java/com/example/m8_projecte_f1_2/MainActivity2.kt
@@ -3,12 +3,17 @@ package com.example.m8_projecte_f1_2
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
@@ -62,8 +67,10 @@ class Quiz {
     data class Pregunta(val pregunta: String, val opciones: Array<String>, val respuestaCorrecta: Int)
 }
 
-
 class QuizActivity : AppCompatActivity() {
+    private lateinit var soundPool: SoundPool
+    private var soundId: Int = 0
+
     private lateinit var quiz: Quiz
     private lateinit var submit_button: Button
     private lateinit var level_text_view: TextView
@@ -72,10 +79,11 @@ class QuizActivity : AppCompatActivity() {
     private lateinit var option_2: RadioButton
     private lateinit var option_3: RadioButton
     private lateinit var option_4: RadioButton
-    private lateinit var score_text_view: TextView  // Agregamos un TextView para mostrar la puntuación
+    private lateinit var score_text_view: TextView
     private var nivelActual = 0
     private var preguntaActual = 0
     private var puntuacion = 0
+    private lateinit var countDownTimer: CountDownTimer
 
     private lateinit var sharedPreferences: SharedPreferences
 
@@ -84,14 +92,28 @@ class QuizActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        // Obtener el nivel seleccionado
+        // Inicializar SoundPool
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .build()
+            soundPool = SoundPool.Builder()
+                .setMaxStreams(1)
+                .setAudioAttributes(audioAttributes)
+                .build()
+        } else {
+            soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+        }
+
+        // Cargar el sonido desde el archivo de recursos
+        soundId = soundPool.load(this, R.raw.f1, 1)
+
         nivelActual = intent.getIntExtra("NIVEL", 0)
 
         sharedPreferences = getSharedPreferences("Puntuacion", Context.MODE_PRIVATE)
-        // Obtener la puntuación del usuario actualmente registrado
         puntuacion = obtenerPuntuacionActual()
 
-        // Inicializar las vistas
         submit_button = findViewById(R.id.submit_button)
         level_text_view = findViewById(R.id.level_text_view)
         question_text_view = findViewById(R.id.question_text_view)
@@ -99,7 +121,7 @@ class QuizActivity : AppCompatActivity() {
         option_2 = findViewById(R.id.option_2)
         option_3 = findViewById(R.id.option_3)
         option_4 = findViewById(R.id.option_4)
-        score_text_view = findViewById(R.id.score_text_view)  // Inicializamos el TextView de la puntuación
+        score_text_view = findViewById(R.id.score_text_view)
 
         level_text_view.text = "Nivel: ${nivelActual + 1}"
         score_text_view.text = "Puntuación: $puntuacion"
@@ -110,9 +132,7 @@ class QuizActivity : AppCompatActivity() {
 
         submit_button.setOnClickListener {
             if (respuestaCorrecta()) {
-                // Asignamos puntos según el nivel de la pregunta
                 puntuacion += nivelActual + 1
-                // Mostramos la puntuación actualizada
                 score_text_view.text = "Puntuación: $puntuacion"
 
                 preguntaActual++
@@ -120,14 +140,21 @@ class QuizActivity : AppCompatActivity() {
                     mostrarPregunta()
                 } else {
                     Toast.makeText(this, "¡Has terminado el nivel!", Toast.LENGTH_SHORT).show()
-                    guardarPuntuacion() // Guardar la puntuación al terminar el nivel
-                    finish() // Finaliza la actividad y vuelve a LevelSelectionActivity
+                    guardarPuntuacion()
+                    soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f) // Reproducir sonido al terminar el nivel
+                    iniciarTemporizador()
                 }
             } else {
                 puntuacion -= nivelActual + 1
                 Toast.makeText(this, "Lo siento, esa respuesta es incorrecta. Intenta de nuevo.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
+        countDownTimer.cancel()
     }
 
     private fun mostrarPregunta() {
@@ -155,7 +182,6 @@ class QuizActivity : AppCompatActivity() {
         editor.putInt("puntuacion", puntuacion)
         editor.apply()
 
-        // Aquí subimos la puntuación a Firebase Realtime Database
         val database = FirebaseDatabase.getInstance("https://m8-projecte-f1-2-default-rtdb.europe-west1.firebasedatabase.app/")
         val uid = FirebaseAuth.getInstance().currentUser?.uid
         val referencia = database.getReference("DATA BASE JUGADORS")
@@ -172,11 +198,24 @@ class QuizActivity : AppCompatActivity() {
     }
 
     private fun obtenerPuntuacionActual(): Int {
-        // Obtener la puntuación del usuario actualmente registrado
         val uid = FirebaseAuth.getInstance().currentUser?.uid
         if (uid != null) {
-            return sharedPreferences.getInt(uid, 0)
+            return sharedPreferences.getInt("puntuacion", 0)
         }
         return 0
+    }
+
+    private fun iniciarTemporizador() {
+        countDownTimer = object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // No se necesita hacer nada en cada tick
+            }
+
+            override fun onFinish() {
+                // Detener la reproducción del sonido
+                soundPool.stop(soundId)
+            }
+        }
+        countDownTimer.start()
     }
 }
